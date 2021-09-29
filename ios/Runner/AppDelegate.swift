b4f05e0892a8dd4f9ec7673d389b4b75a009fd7e
Vong Nyuksoon <vongnyuksoon@Vongs-MacBook-Air.local>
2021-09-29 17:35:08 +0800

    deps: Migrate dependencies for flutter 2.5

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,20 +6,19 @@ import flutter_config
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        FirebaseApp.configure()
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    FirebaseApp.configure()
         
         if let apiKey = FlutterConfigPlugin.env(for: "IOS_MAPS_API_KEY") {
             GMSServices.provideAPIKey(apiKey)
         }
-        GeneratedPluginRegistrant.register(with: self)
-        
-        if #available(iOS 10.0, *) {
+    GeneratedPluginRegistrant.register(with: self)
+    if #available(iOS 10.0, *) {
             UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
         }
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
